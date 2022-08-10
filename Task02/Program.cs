1e324cd47ac246f348c85cd369d6e63f3c6e0b83
Elena Potapkina <epotapkina.1@gmail.com>
2022-08-10 16:02:55 +0300

    add Task002_003 and folders

---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на выход принимает два числа и выдает, какое число большее,а какое меньшее
+// а = 5, b=7
+// а = -2, b = 10
+// а = -9, b = -3
+Console.WriteLine("Введите первое целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine ($"большее число: {number1}, меньшее число: {number2}");
+} else
+{
+ Console.WriteLine ($"большее число: {number2}, меньшее число: {number1}");   
+}
--- Task03/Program.cs
+++ Task03/Program.cs
@@ -2,4 +2,18 @@
 // название дня недели по заданному номеру.
 // 3 -> Среда
 // 5 -> Пятница
-Console.WriteLine("Hello, World!");
+Console.Write("Введите целое число соответствующее названию дня недели (от 1 до 7): ");;
+int number = Convert.ToInt32(Console.ReadLine ( ));
+string day = "";
+if (number>=1 && number<=7)
+{switch (number)
+{case 1:
+day = "Понедельник";
+break;
+    
+    default:
+    day = ("Вы ввели некорректное число");
+    break;
+}
+    Console.WriteLine ("День недели:" + day);
+}
