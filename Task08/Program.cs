255d41ffa6757856c4ac438924da38f62064769d
Elena Potapkina <epotapkina.1@gmail.com>
2022-08-11 09:39:18 +0300

    add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int i = 1;
+int result = 0;
+int Array = (number);
+while(i<=number)
+{{if (i%2==0)
+    {result = i;}
+};
+i=i+1;}
+Console.WriteLine($"Эти числа в диапозоне от 1 до {number} является четными: {result}");
