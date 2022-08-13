02499514366fe0e7348596a69f26f86eadecaa82
Elena Potapkina <epotapkina.1@gmail.com>
2022-08-13 22:12:01 +0300

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");;
+int num1= Convert.ToInt32(Console.ReadLine ( ));
+
+
+bool CheckingQuad (int a)
+
+{
+return (a%7==0 && a%23==0);
+
+}
+
+bool result = CheckingQuad(num1);
+Console.Write(result? "да" : "нет");
+
