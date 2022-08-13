7ba50ca0d5b4e5f27ed6bf363e0db5a63d612533
Elena Potapkina <epotapkina.1@gmail.com>
2022-08-14 01:10:34 +0300

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число от 1 до 7: ");;
+int num1= Convert.ToInt32(Console.ReadLine ( ));
+
+ bool DayChecking(int a)
+ {
+    return (a==6 || a==7);
+ }
+
+bool result = DayChecking(num1);
+Console.Write(result? "да" : "нет");
